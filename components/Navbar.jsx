4a6cc7a8e65315597b0181f4aa44353a73467eec
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-22 17:50:26 +0100

    eslint

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,18 @@
 'use client';
-import {motion} from 'framer-motion';
+
+import { motion } from 'framer-motion';
+
 import styles from '../styles';
-import {navVariants} from '../utils/motion';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <motion.nav variants={navVariants}
-  initial="hidden" 
-  whileInView="show"
-  className={`${styles.xPaddings} py-8 relative`}
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
   >
-    navbar
+    <div className="absolute w-[50%] inset-0 gradient-01" />
   </motion.nav>
 );
 
