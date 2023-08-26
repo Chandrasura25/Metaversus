200feccbb6143ba692e960299ee132e084df37ad
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-26 19:59:55 +0100

    explore

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -6,22 +6,38 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { staggerContainer } from '../utils/motion';
 import { TypingText, ExploreCard, TitleText } from '../components';
-import {exploreWorlds} from '../constants';
+import { exploreWorlds } from '../constants';
 
-
-const Explore = () => (
-  <section id="explore" className={`${styles.paddings}`}>
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{once: false, amount: 0.25}}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-      <TypingText title="| The World" textStyles="text-center" />
-      <TitleText title={<>Choose the world you want <br className="md:block hidden" /> to explore </>} textStyles="text-center" />
-    </motion.div>
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState(0);
+  return (
+    <section id="explore" className={`${styles.paddings}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={<>Choose the world you want <br className="md:block hidden" /> to explore </>}
+          textStyles="text-center"
+        />
+        <div className="flex mt-[50px] lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
