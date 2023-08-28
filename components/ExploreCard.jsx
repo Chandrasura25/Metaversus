ec7722999a8e872f50131048a4f916ed96ca8b2b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-28 17:37:10 +0100

    explore card

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -6,9 +6,12 @@ import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({active, index, handleClick, id, imgUrl, title}) => (
-  <div>
+  <motion.div 
+    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+    className={`relative ${active === id ? 'lg:flex-[0.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'} flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+  >
     Explore Card
-  </div>
+  </motion.div>
 );
 
 export default ExploreCard;
