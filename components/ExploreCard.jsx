1d768f57dd1c7eaf48c293caf6be88f76f23a816
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-28 17:32:05 +0100

    explore card

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
-const ExploreCard = () => (
+const ExploreCard = ({active, index, handleClick, id, imgUrl, title}) => (
   <div>
     Explore Card
   </div>
