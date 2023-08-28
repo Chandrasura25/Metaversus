e94aa147c64fd4d8881a8900adb4699650569cd7
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-28 18:22:38 +0100

    get started

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -13,14 +13,21 @@ const GetStarted = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{once: 'false', amount: 0.25}}
+      viewport={{ once: 'false', amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
         variants={planetVariants('left')}
         className={`flex-1 ${styles.flexCenter}`}
       >
-        <img/>
+        <img src="/get-started.png" alt="get_started" className="w-[90%] h-[90%] object-contain" />
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="| How Metaversus Works" />
+        <TitleText title={<>Get started with just a few clicks</>} />
       </motion.div>
     </motion.div>
   </section>
