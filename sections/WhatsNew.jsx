f82879e5a15324449c4e948476012f35dcd51642
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-28 19:40:55 +0100

    get started

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 
 import styles from '../styles';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
-import { TitleText, TypingText, StartSteps } from '../components';
-import { startingFeatures } from '../constants';
+import { TitleText, TypingText, NewFeatures } from '../components';
+import { newFeatures } from '../constants';
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -17,24 +17,24 @@ const WhatsNew = () => (
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)}
+        variants={fadeIn('right', 'tween', 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
-        <TypingText title="| How Metaversus Works" />
-        <TitleText title={<>Get started with just a few clicks</>} />
-        {/* <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
-          {startingFeatures.map((feature, index) => (
-            <StartSteps key={feature} number={index + 1} text={feature} />
+        <TypingText title="| What's New ?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {newFeatures.map((feature, index) => (
+            <NewFeatures key={feature} number={index + 1} text={feature} />
           ))}
-        </div> */}
+        </div>
       </motion.div>
       <motion.div
         variants={planetVariants('right')}
         className={`flex-1 ${styles.flexCenter}`}
       >
         <img
-          src="/get-started.png"
-          alt="get_started"
+          src="/whats-new.png"
+          alt="what_new"
           className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
