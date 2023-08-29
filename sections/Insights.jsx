d38d3884dec6c05b10bc3b0f1b9972adbd927fdb
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-29 20:28:53 +0100

    world

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { TitleText, TypingText, NewFeatures } from '../components';
+import { newFeatures } from '../constants';
+
 const Insights = () => (
   <section>
     Insights section
