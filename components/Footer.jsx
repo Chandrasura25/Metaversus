ace49c874c7bb06b1c4f093bfa91f1d32ffa1c19
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-30 19:08:19 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,6 +14,12 @@ const Footer = () => (
     whileInView="show"
   >
     <div className="footer-gradient" />
+
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="">Enter the Metaverse</h4>
+      </div>
+    </div>
   </motion.footer>
 );
 
