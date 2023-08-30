edf8d95ade925f86451a405891f0a386ce80eed4
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-30 19:10:19 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,7 +17,10 @@ const Footer = () => (
 
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="">Enter the Metaverse</h4>
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">Enter the Metaverse</h4>
+        <button type="button" className="flex items-center h0fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]">
+          <img src="/headset.svg" alt="headset" />
+        </button>
       </div>
     </div>
   </motion.footer>
