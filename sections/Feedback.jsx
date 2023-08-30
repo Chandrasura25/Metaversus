b3b6a47075be2b977f8946df3d4be2da90581e3c
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-30 18:54:16 +0100

    feedback

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -19,6 +19,19 @@ const Feedback = () => (
         className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
       >
         <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+            Samantha
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | Metaversus
+          </p>
+        </div>
+        <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+          "With the development of today's technology, metaverse is very useful
+          for today's work, or can be called web 3.0. by using metaverse you can
+          use it as anything"
+        </p>
       </motion.div>
     </motion.div>
   </section>
